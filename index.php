ff747bbb66b97d35f40c054a4b2eb56a135674c4
NotAlex575 <dapontealessandro89gmail.com>
2025-10-30 18:37:28 +0100

    MILESTONE 1 COMPLETATA: Creato il sistema di generazione randomica (tra caratteri minuscoli, maiuscoli e numeri ripetibili) e stampa password (se esiste) nella pagina se il campo password è 0, oppure <5 oppure >36, non fa mettere la password (per dargli almeno una lunghezza minima e massima)

--- index.php
+++ index.php
@@ -28,10 +28,15 @@
         </div>
         <?php
         $passwordLength = $_GET["passwordLength"]?? "";
-        var_dump($passwordLength);
         if($passwordLength != null){
+            $password = "";
+            $caratteri = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            for($i = 0; $i < $passwordLength; $i++){
+                $carattere = rand(0, strlen($caratteri)-1);
+                $password .= $caratteri[$carattere];
+            }
             echo "<div class='col-md-12 mt-5'>
-                Ciao, ecco qui la tua password! ". $passwordLength ."!
+                Ciao, ecco qui la tua password! ". $password ."!
             </div>";
         }
         ?>
