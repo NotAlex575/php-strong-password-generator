05c8605fac57c06f1a814c863210079769e65aad
NotAlex575 <dapontealessandro89gmail.com>
2025-10-30 18:40:22 +0100

    MILESTONE 2 COMPLETATA: Creata un nuovo file (function.php), che contiene la logica della generazione della password e della sua stampa sulla pagina

--- index.php
+++ index.php
@@ -26,19 +26,8 @@
             <button class="btn btn-primary w-100" type="submit">Invia</button>
           </form>
         </div>
-        <?php
-        $passwordLength = $_GET["passwordLength"]?? "";
-        if($passwordLength != null){
-            $password = "";
-            $caratteri = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            for($i = 0; $i < $passwordLength; $i++){
-                $carattere = rand(0, strlen($caratteri)-1);
-                $password .= $caratteri[$carattere];
-            }
-            echo "<div class='col-md-12 mt-5'>
-                Ciao, ecco qui la tua password! ". $password ."!
-            </div>";
-        }
+        <?php 
+            include_once "function.php" 
         ?>
       </div>
     </div>
