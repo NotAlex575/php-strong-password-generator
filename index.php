9a21c29f6618f31e2534abd39d3b353f24d37472
NotAlex575 <dapontealessandro89gmail.com>
2025-10-31 12:48:33 +0100

    aggiunto button per poi fare il check della password per evitare o meno caratteri duplicati

--- index.php
+++ index.php
@@ -13,6 +13,13 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
+<style>
+  .checkbox-size{
+    transform: scale(1.2);
+    margin-left: 10px; 
+  }
+</style>
+
 <body class="bg-light">
     <header class="text-center mt-4">
         <h1>PASSWORD GENERATOR SIMULATOR!</h1>
@@ -21,12 +28,21 @@
   <main class="d-flex justify-content-center align-items-center vh-100">
     <div class="container text-center">
       <div class="row justify-content-center">
-        <div class="col-md-4">
+        <div class="col-md-5">
           <form action="index.php" method="post" class="p-4 border rounded bg-white shadow-sm">
+            
+            <!-- LUNGHEZZA PASSWORD -->
             <div class="mb-3">
               <label class="form-label" for="passwordLength">Lunghezza password</label>
               <input class="form-control" type="number" min="4" max="36" name="passwordLength" id="passwordLength">
             </div>
+
+            <!-- DUPLICATI? -->
+            <div class="mt-3 mb-3">
+              <label for="form-label" for="duplicati">Non aggiungere caratteri duplicati</label>
+              <input class="checkbox-size" type="checkbox" class="ms-2" name="duplicati">
+            </div>
+
             <button class="btn btn-primary w-100" type="submit">Invia</button>
           </form>
         </div>
